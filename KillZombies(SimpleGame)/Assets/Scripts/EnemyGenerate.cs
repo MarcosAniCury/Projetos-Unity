13e796a9f4c5f683ffc9bbe01b225c87583f8de8
MarcosAniCury <curymarcos@live.com>
2022-03-03 13:21:41 -0300

    Fixed camera position and improve enemy generate

--- KillZombies(SimpleGame)/Assets/Scripts/EnemyGenerate.cs
+++ KillZombies(SimpleGame)/Assets/Scripts/EnemyGenerate.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemyGenerate : MonoBehaviour
@@ -5,18 +6,62 @@ public class EnemyGenerate : MonoBehaviour
     //Public vars
     public GameObject Zombie;
     public float GenerateEnemyTime = 1;
+    public LayerMask LayerEnemys;
 
     //Private vars
     private float timeCount;
 
-    // Update is called once per frame
+    //Components
+    GameObject player;
+
+    //CONSTs
+    const int RADIO_TO_GENERATE_RANDOM_POSITION = 3;
+    const int DISTANCE_BETWEEN_PLAYER_AND_ENEMY_TO_SPAWN = 20;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag(Constants.TAG_PLAYER);
+    }
+
     void Update()
     {
-        timeCount += Time.deltaTime;
+        if (Vector3.Distance(
+                transform.position, 
+                player.transform.position
+            ) > DISTANCE_BETWEEN_PLAYER_AND_ENEMY_TO_SPAWN) {
+            timeCount += Time.deltaTime;
 
-        if (timeCount > GenerateEnemyTime) {
-            Instantiate(Zombie, transform.position, transform.rotation);
-            timeCount = 0;
+            if (timeCount > GenerateEnemyTime) {
+                StartCoroutine(GenerateZombie());
+                timeCount = 0;
+            }
         }
+    }
+
+    IEnumerator GenerateZombie()
+    {
+        Vector3 randomPosition;
+        Collider[] colliders;
+        do {
+            randomPosition = GenerateRandomPosition();
+            colliders = Physics.OverlapSphere(randomPosition, 1, LayerEnemys);
+            yield return null;
+        } while (colliders.Length > 0);
+        Instantiate(Zombie, randomPosition, transform.rotation);
+    }
+
+    Vector3 GenerateRandomPosition()
+    {
+        Vector3 position = Random.insideUnitSphere * RADIO_TO_GENERATE_RANDOM_POSITION;
+        position += transform.position;
+        position.y = transform.position.y;
+
+        return position;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, RADIO_TO_GENERATE_RANDOM_POSITION);
     }
 }
