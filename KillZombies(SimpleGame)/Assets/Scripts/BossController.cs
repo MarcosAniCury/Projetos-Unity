87100a851cff5892073a9c2c70353f54dd5d3c64
Marcos Ani Cury Vinagre Silva <65677865+MarcosAniCury@users.noreply.github.com>
2022-04-14 18:30:25 -0300

    Put animation on boss

--- KillZombies(SimpleGame)/Assets/Scripts/BossController.cs
+++ KillZombies(SimpleGame)/Assets/Scripts/BossController.cs
@@ -4,18 +4,35 @@ using UnityEngine.AI;
 
 public class BossController : MonoBehaviour
 {
-    //Private vars
-    private Transform player;
-    private NavMeshAgent agent;
+    //COMPONENTs
+    Transform player;
+    NavMeshAgent agent;
+    Status myStatus;
+    AnimationCharacter myAnimation;
+    MovementCharacter myMovement;
 
     private void Start()
     {
         player = GameObject.FindWithTag(Constants.TAG_PLAYER).transform;
         agent = GetComponent<NavMeshAgent>();
+        myStatus = GetComponent<Status>();
+        agent.speed = myStatus.Speed;
+        myAnimation = GetComponent<AnimationCharacter>();
+        myMovement = GetComponent<MovementCharacter>();
     }
 
     private void Update()
     {
         agent.SetDestination(player.position);
+        myAnimation.Walk(agent.velocity.magnitude);
+
+        if (agent.hasPath)
+        {
+            bool playerClose = agent.remainingDistance <= agent.stoppingDistance;
+            myAnimation.Attack(playerClose);
+            
+            Vector3 direction = player.position - transform.position;
+            myMovement.Rotation(direction);
+        }
     }
 }
