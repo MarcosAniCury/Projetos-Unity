868177858303cb92929f40d3ef993874702a6355
Marcos Ani Cury Vinagre Silva <65677865+MarcosAniCury@users.noreply.github.com>
2022-04-14 17:27:24 -0300

    Add boss navMesh movimentation

---
+++ KillZombies(SimpleGame)/Assets/Scripts/BossController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BossController : MonoBehaviour
+{
+    //Private vars
+    private Transform player;
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag(Constants.TAG_PLAYER).transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        agent.SetDestination(player.position);
+    }
+}
